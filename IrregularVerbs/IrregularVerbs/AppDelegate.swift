5697ecd4880a9832cccc1cc920c73c2e79096ac1
Marat Cherkasov <marat.cherkasov@yandex.ru>
2023-04-20 23:33:15 +0500

    initial commit

---
+++ IrregularVerbs/IrregularVerbs/AppDelegate.swift
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.swift
+//  IrregularVerbs
+//
+//  Created by Marat Cherkasov on 20.04.2023.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        
+        if let window = window {
+            let navigationController = UINavigationController()
+            navigationController.viewControllers = [ViewController()]
+            window.rootViewController = navigationController
+            window.makeKeyAndVisible()
+        }
+        
+        return true
+    }
+}
---
+++ IrregularVerbs/IrregularVerbs/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  IrregularVerbs
+//
+//  Created by Marat Cherkasov on 20.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
